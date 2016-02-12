03534c8497c7f2658abc562e8808b6ee1dd36dd1
David Mendez <desiratech@gmail.com>
2016-02-11 20:05:23 -0600

    enter and update added

--- src/classes/play-state.ts
+++ src/classes/play-state.ts
@@ -1,41 +1,68 @@
 import boldInvaders = require('./bold-invaders');
 class PlayState {
 
-    constructor(game: boldInvaders,
-        playerOptions: BoldInvaders.Player,
-        enemyOptions: BoldInvaders.Enemy,
-        playStateOptions: BoldInvaders.PlayState) {
+    constructor(public game: boldInvaders,
+       public playerOptions: BoldInvaders.Player,
+       public enemyOptions: BoldInvaders.Enemy,
+       public playStateOptions: BoldInvaders.PlayState) {
 
     }
 
-    enter(game: boldInvaders,
-        playerOptions: BoldInvaders.Player,
-        enemyOptions: BoldInvaders.Enemy,
-        playStateOptions: BoldInvaders.PlayState) {
+    enter() {
         
         //instantiate le ship
-        playStateOptions.ship = { x: game.stateOptions.width / 2, y: game.stateOptions.gameBounds.bottom, width: 20, height: 16 };
+        this.playStateOptions.ship = { x: this.game.stateOptions.width / 2, y: this.game.stateOptions.gameBounds.bottom, width: 20, height: 16 };
         
         //create level multipliers
-        var levelMultiplier = game.stateOptions.level * game.boldOptions.levelDifficultyMultiplier;
-        var shipSpeed = game.boldOptions.shipSpeed;
-        enemyOptions.invaderInitialVelocity = 25 + (levelMultiplier * 25);
-        enemyOptions.bombRate = 0.05 + (levelMultiplier * 0.05);
-        enemyOptions.bombMinVelocity = 50 + (levelMultiplier * 50);
-        enemyOptions.bombMaxVelocity = 60 + (levelMultiplier * 60);
+        var levelMultiplier: number = this.game.stateOptions.level * this.game.boldOptions.levelDifficultyMultiplier;
+        var shipSpeed: number = this.game.boldOptions.shipSpeed;
+        this.enemyOptions.invaderInitialVelocity = 25 + (levelMultiplier * 25);
+        this.enemyOptions.bombRate = 0.05 + (levelMultiplier * 0.05);
+        this.enemyOptions.bombMinVelocity = 50 + (levelMultiplier * 50);
+        this.enemyOptions.bombMaxVelocity = 60 + (levelMultiplier * 60);
         
         //invader creation
-        enemyOptions.invaderRanks = 5;
-        enemyOptions.invaderFiles = 10;
-        var invaders = [];
-        for (var rank = 0; rank < enemyOptions.invaderRanks; rank++) {
-            for (var file = 0; file < enemyOptions.invaderFiles; file++) {
-                var invader: BoldInvaders.Invader = {x: (game.stateOptions.width / 2) + ((enemyOptions.invaderFiles / 2 - file) * 200 / enemyOptions.invaderFiles),
-                    y: (game.stateOptions.gameBounds.top + rank * 20), rank: rank, file: file, type: 'Invader', width: 18, height: 14 }
+        this.enemyOptions.invaderRanks = 5;
+        this.enemyOptions.invaderFiles = 10;
+        var invaders: Array<BoldInvaders.Invader> = [];
+        for (var rank = 0; rank < this.enemyOptions.invaderRanks; rank++) {
+            
+            for (var file = 0; file < this.enemyOptions.invaderFiles; file++) {
+                
+                var invader: BoldInvaders.Invader = {x: (this.game.stateOptions.width / 2) + ((this.enemyOptions.invaderFiles / 2 - file) * 200 / this.enemyOptions.invaderFiles),
+                    y: (this.game.stateOptions.gameBounds.top + rank * 20), rank: rank, file: file, type: 'Invader', width: 18, height: 14 };
+                    
                 invaders.push(invader);
             }
         }
+        
+        this.playStateOptions.invaders = invaders;
+        this.playStateOptions.invaderCurrentVelocity = this.enemyOptions.invaderInitialVelocity;
+        var invaderVelocity = {x: this.enemyOptions.invaderInitialVelocity, y:0};
+        var invaderNextVelocity = null;
+        
 
+    }
+    
+    update(){
+        if(this.game.stateOptions.pressedKeys[37]) {
+        this.playStateOptions.ship.x -= this.game.boldOptions.shipSpeed * 1000 / this.game.boldOptions.fps;
+    }
+    if(this.game.stateOptions.pressedKeys[39]) {
+        this.playStateOptions.ship.x += this.game.boldOptions.shipSpeed * 1000 / this.game.boldOptions.fps;
+    }
+    if(this.game.stateOptions.pressedKeys[32]) {
+        //this.fireRocket();
+        console.log("fire rocket");
+    }
+ 
+    //  Keep the ship in bounds.
+    if(this.playStateOptions.ship.x < this.game.stateOptions.gameBounds.left) {
+        this.playStateOptions.ship.x = this.game.stateOptions.gameBounds.left;
+    }
+    if(this.playStateOptions.ship.x > this.game.stateOptions.gameBounds.right) {
+        this.playStateOptions.ship.x = this.game.stateOptions.gameBounds.right;
+    }
     }
 
 
